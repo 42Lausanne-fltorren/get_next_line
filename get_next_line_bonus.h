57253d802f1e0afa1b88a3eee3040b2710ecfee6
Torrenté Florian <florian@deneria.net>
2023-11-11 19:17:29 +0100

    Final commit ?

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fltorren <fltorren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:04:19 by fltorren          #+#    #+#             */
-/*   Updated: 2023/11/10 20:21:24 by fltorren         ###   ########.fr       */
+/*   Updated: 2023/11/11 19:13:47 by fltorren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <fcntl.h>
-# define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
